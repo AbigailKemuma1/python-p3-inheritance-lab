51754be1633da5b5ae511b5ba4b46d3b4fd58aa9
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-08-24 03:21:37 +0300

    Adds student, teacher and user.py

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, teacher=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        self.teacher = teacher
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        """Accept a new piece of knowledge and add it to the knowledge list"""
+        self.knowledge.append(new_knowledge)
+        
